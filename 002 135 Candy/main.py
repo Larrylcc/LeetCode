d60ce7cdde3afbcf69795584e4073ad36fd1b2cd
Chen Lu <110882079+Larrylcc@users.noreply.github.com>
2025-09-30 16:41:03 +0800

    135 Candy solved

---
+++ 002 135 Candy/main.py
@@ -0,0 +1,10 @@
+from Solution import Solution
+
+def main():
+    solution_instance=Solution()
+    ratings =[1, 0, 2]
+    result = solution_instance.candy(ratings)
+    print(result)
+
+if __name__ == '__main__':
+    main()
