ce4fed677657f05072850afe12b803056d64c187
Chen Lu <110882079+Larrylcc@users.noreply.github.com>
2025-10-06 22:45:08 +0800

    763 Paratition Labels solved

--- Greedy Algorithm/Practice/003 763 Partition Labels/Solution.py
+++ Greedy Algorithm/Practice/003 763 Partition Labels/Solution.py
@@ -1,3 +1,15 @@
 from typing import List
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
+        alpha_dict= {}
+        for i,char in enumerate(s):
+            alpha_dict[char]=i
+        result=[]
+        start=0
+        end=0
+        for i,char in enumerate(s):
+            end=max(end,alpha_dict[char])
+            if end == i:
+                result.append(end-start+1)
+                start=i+1
+        return result
