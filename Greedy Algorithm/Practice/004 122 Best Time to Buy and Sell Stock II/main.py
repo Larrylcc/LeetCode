2ece073bae47ba5a9695bba04a7d9efb418a698e
Chen Lu <110882079+Larrylcc@users.noreply.github.com>
2025-10-07 15:24:15 +0800

    122 Best Time to Buy and Sell Stock II init

---
+++ Greedy Algorithm/Practice/004 122 Best Time to Buy and Sell Stock II/main.py
@@ -0,0 +1,10 @@
+from Solution import Solution
+
+def main():
+    solution_instance=Solution()
+    var=[7,1,5,3,6,4]
+    result = solution_instance.partitionLabels(var)
+    print(result)
+
+if __name__ == '__main__':
+    main()
