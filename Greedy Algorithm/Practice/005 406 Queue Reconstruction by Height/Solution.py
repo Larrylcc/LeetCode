b324bf875aaa8ba557ed67e19985d065c81fdc7e
Chen Lu <110882079+Larrylcc@users.noreply.github.com>
2025-10-31 20:17:59 +0800

    406 solved

--- Greedy Algorithm/Practice/005 406 Queue Reconstruction by Height/Solution.py
+++ Greedy Algorithm/Practice/005 406 Queue Reconstruction by Height/Solution.py
@@ -2,4 +2,8 @@ from typing import List
 class Solution:
     class Solution:
         def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-            return 0
+            people.sort(key=lambda x: (-x[0], x[1]))
+            ans: List[List[int]] = []
+            for h, k in people:
+                ans.insert(k, [h, k])
+            return ans
