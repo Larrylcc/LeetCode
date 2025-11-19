be00334cc10f3c70abc3b211f682878157416322
Lu Chen <110882079+Larrylcc@users.noreply.github.com>
2025-11-19 23:57:13 +0800

    76 init

--- Two Pointer/003 76 Minimum Window Substring/main.py
+++ Two Pointer/003 76 Minimum Window Substring/main.py
@@ -1,13 +1,13 @@
 from Solution import Solution
 
-def main():
-    solution_instance=Solution()
-    nums1=[2,0]
-    m=1
-    nums2=[1]
-    n=1
-    solution_instance.merge(nums1,m,nums2,n)
-    print(nums1)
 
-if __name__ == '__main__':
+def main():
+    solution_instance = Solution()
+    s = "ADOBECODEBANC"
+    t = "ABC"
+    str = solution_instance.minWindow(s, t)
+    print(str)
+
+
+if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
-    array =[[10,16],[2,8],[1,6],[7,12]]
+    array = [[10, 16], [2, 8], [1, 6], [7, 12]]
     print(len(array))
+
 
 if __name__ == "__main__":
     main()
